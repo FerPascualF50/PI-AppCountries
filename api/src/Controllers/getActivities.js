45e5d510a3053da9222de6245f5fd082d714cf8b
FerPascualF50 <pascualefernando@gmail.com>
2022-09-14 17:11:13 -0300

    FirteronGetActivityBugFixed

--- api/src/Controllers/getActivities.js
+++ api/src/Controllers/getActivities.js
@@ -4,6 +4,7 @@ const getActivities = async (req, res) => {
     try {
         const activities = await Activity.findAll()
         if (activities) {
+            activities.filter(e => e.mane)
             res.json(activities)
         } else {
             res.status(404).send("Activity not found")
@@ -12,4 +13,5 @@ const getActivities = async (req, res) => {
         res.status(404).json(error)
     }
 }
+console.log(getActivities.activities)
 module.exports = { getActivities }
