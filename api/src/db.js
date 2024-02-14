f3d9cc1f4d4d6d7bc14c3567cf58c19e4a28019a
Fer Pascual <pascualefernando@gmail.com>
2024-02-14 01:03:22 -0300

    ok

--- api/src/db.js
+++ api/src/db.js
@@ -5,36 +5,36 @@ const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 let sequelize =
-  process.env.NODE_ENV === "production"
-     ? 
-    new Sequelize({
-      database: DB_NAME,
-      dialect: "postgres",
-      host: DB_HOST,
-      port: 5432,
-      username: DB_USER,
-      password: DB_PASSWORD,
-      pool: {
-        max: 3,
-        min: 1,
-        idle: 10000,
-      },
-      dialectOptions: {
-        ssl: {
-          require: true,
-          // Ref.: https://github.com/brianc/node-postgres/issues/2009
-          rejectUnauthorized: false,
-        },
-        keepAlive: true,
-      },
-      ssl: true,
-    })
-    : 
+  // process.env.NODE_ENV === "production"
+  //    ? 
+  //   new Sequelize({
+  //     database: DB_NAME,
+  //     dialect: "postgres",
+  //     host: DB_HOST,
+  //     port: 5432,
+  //     username: DB_USER,
+  //     password: DB_PASSWORD,
+  //     pool: {
+  //       max: 3,
+  //       min: 1,
+  //       idle: 10000,
+  //     },
+  //     dialectOptions: {
+  //       ssl: {
+  //         require: true,
+  //         // Ref.: https://github.com/brianc/node-postgres/issues/2009
+  //         rejectUnauthorized: false,
+  //       },
+  //       keepAlive: true,
+  //     },
+  //     ssl: true,
+  //   })
+  //   : 
     new Sequelize(
       //`postgres://postgres123:aYAZuOTOfdh1U3rVHMx6LXPYK4FgrcRY@dpg-cn62heqcn0vc73daaapg-a.oregon-postgres.render.com/dbcountry_i6p6`
       `postgres://postgres123:aYAZuOTOfdh1U3rVHMx6LXPYK4FgrcRY@dpg-cn62heqcn0vc73daaapg-a/dbcountry_i6p6`,
       //`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dbcountry_i6p6`,
-      { logging: false, native: false }
+      // { logging: false, native: false }
     );
 
 
